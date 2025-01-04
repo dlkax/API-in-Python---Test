b29db3e94000eb3a7e8c0b604bb56f27a1a09ff3
Diego Buzo <90202864+dlkax@users.noreply.github.com>
2025-01-04 23:42:39 +0000

    import/add database.SQLALCHEMY

--- api com python/app.py
+++ api com python/app.py
@@ -1,11 +1,14 @@
 #importação
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 #criando uma variavel (app) que irá receber a instancia
 #da classe (Flask)
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+db = SQLAlchemy(app)
 #definir uma rota raiz (pagina inicial) e a função 
 #que será executada ao requisitar 
 @app.route('/teste')
