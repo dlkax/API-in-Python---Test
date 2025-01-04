dfa9a5d5ee9f7a6f2d39e5759871c1f1df9719c6
Diego Buzo <90202864+dlkax@users.noreply.github.com>
2025-01-04 23:52:58 +0000

    Class Created - id, name, price and description

--- api com python/app.py
+++ api com python/app.py
@@ -9,6 +9,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+
+# modelagem 
+# produto (id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
 #definir uma rota raiz (pagina inicial) e a função 
 #que será executada ao requisitar 
 @app.route('/teste')
